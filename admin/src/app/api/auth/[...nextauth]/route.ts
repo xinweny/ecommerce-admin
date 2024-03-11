68668b4e2892f327529fc9beb6bca9536a0bb20e
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-03-11 12:55:05 -0400

    add auth middleware and config

--- admin/src/app/api/auth/[...nextauth]/route.ts
+++ admin/src/app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,1 @@
-import NextAuth from "next-auth/next";
-import Email from "next-auth/providers/email";
-
-export const authOptions = {
-  secret: process.env.NEXTAUTH_SECRET,
-  providers: [
-    Email({
-
-    }),
-  ],
-};
-
-const handler = NextAuth(authOptions);
-
-export {
-  handler as GET,
-  handler as POST,
-};
+export { GET, POST } from "@/auth";
--- admin/src/middleware.ts
+++ admin/src/middleware.ts
@@ -1,5 +1,38 @@
-export { default } from "next-auth/middleware";
+import NextAuth from "next-auth";
+
+import authConfig from "./auth.config";
+
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
+} from "@/routes";
+import { NextRequest } from "next/server";
+
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  if (nextUrl.pathname.startsWith(apiAuthPrefix)) return;
+  
+  if (authRoutes.includes(nextUrl.pathname)) {
+    return isLoggedIn
+      ? Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+      : undefined;
+  }
+
+  if (!isLoggedIn && !(publicRoutes.includes(nextUrl.pathname))) return Response.redirect(new URL("/auth/login", nextUrl));
+
+  return;
+});
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: [
+    "/((?!.+\\.[\\w]+$|_next).*)",
+    "/",
+    "/(api|trpc)(.*)",
+  ],
 };
