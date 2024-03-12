8dbc4d39f71e5c2476c89bd433eaa7d61a3b62fa
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-03-12 11:56:18 -0400

    add socials buttons

--- store/src/app/(routes)/(auth)/login/_components/social.tsx
+++ store/src/app/(routes)/(auth)/login/_components/social.tsx
@@ -1,11 +1,29 @@
 "use client";
 
+import { signIn } from "next-auth/react";
+
+import { DEFAULT_LOGIN_REDIRECT } from "@/lib/routes";
+
+import { Button } from "@/components/ui/button";
+
 import { FcGoogle } from "react-icons/fc";
 
 export function Social() {
+  const onClick = (provider: "google") => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    });
+  };
+
   return (
     <div className="flex items-center w-full gap-x-2">
-      
+      <Button
+        size="lg"
+        onClick={() => { onClick("google"); }}
+        variant="outline"
+      >
+        <FcGoogle className="h-5 w-5" />
+      </Button>
     </div>
   );
 }
