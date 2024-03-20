2a526a147c0f7a241c46ec7730016fb8d675048b
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-03-20 14:26:06 -0400

    add store model

--- admin/src/app/(routes)/(protected)/dashboard/_components/create-store-form.tsx
+++ admin/src/app/(routes)/(protected)/dashboard/_components/create-store-form.tsx
@@ -4,9 +4,22 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 import { createStoreSchema } from "@/schemas/store";
 
-import { Form } from "@/components/ui/form";
+import { useStoreModal } from "@/hooks";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { SubmitButton } from "@/components/form/submit-button";
 
 export function CreateStoreForm() {
+  const { onClose } = useStoreModal();
+
   const form = useForm<z.infer<typeof createStoreSchema>>({
     resolver: zodResolver(createStoreSchema)
   });
@@ -18,8 +31,30 @@ export function CreateStoreForm() {
   return (
     <div>
       <div className="space-y-4 py-2 pb-4">
-        <Form>
-
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Piano"
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+              <Button variant="outline" onClick={onClose}>
+                Cancel
+              </Button>
+              <SubmitButton>Create</SubmitButton>
+            </div>
+          </form>
         </Form>
       </div>
     </div>
