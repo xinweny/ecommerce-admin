9af43605ce8fe1996e491735d814fc3bd88f4514
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-03-20 23:22:11 -0400

    set up store dashboard page

--- admin/src/app/(routes)/(protected)/dashboard/[storeId]/page.tsx
+++ admin/src/app/(routes)/(protected)/dashboard/[storeId]/page.tsx
@@ -1,7 +1,15 @@
-export default function StorePage() {
+import { getStoreById } from "@/db/query/store";
+
+interface DashboardStorePageProps {
+  params: { storeId: string };
+}
+
+export default async function DashboardStorePage({ params }: DashboardStorePageProps) {
+  const store = await getStoreById(params.storeId);
+
   return (
     <div>
-
+      Store ${store?.name}
     </div>
   );
 }
