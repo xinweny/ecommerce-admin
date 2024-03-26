a738a533bcd6ff34ff5212ed2e32733103661f26
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-03-26 12:55:57 -0400

    restyle nav bar

--- admin/src/app/(routes)/(protected)/dashboard/_components/main-nav.tsx
+++ admin/src/app/(routes)/(protected)/dashboard/_components/main-nav.tsx
@@ -1,53 +1,108 @@
 "use client";
 
-import { usePathname, useParams } from "next/navigation";
+import { useParams } from "next/navigation";
 import Link from "next/link";
 
-import { cn } from "@/lib/utils";
+import { useIsMounted } from "@/hooks";
 
+import {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+  NavigationMenuTrigger,
+  NavigationMenuContent,
+  NavigationMenuLink,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
 
-export function MainNav({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLElement>) {
-  const pathname = usePathname();
+export function MainNav() {
   const { storeId } = useParams();
+  
+  const isMounted = useIsMounted();
 
-  const routes = [
-    {
-      href: storeId ? `/${storeId}` : "/",
-      label: "Overview",
-    },
-  ];
-
-  if (storeId) routes.push(...[
-    {
-      href: `/${storeId}/display`,
-      label: "Display",
-    },
-    {
-      href: `/${storeId}/settings`,
-      label: "Settings",
-    },
-  ]);
+  if (!isMounted) return null;
 
   return (
-    <nav
-      className={cn("flex items-center space-x-4 lg:space-x-6", className)}
-      {...props}
-    >
-      {routes.map(({ href, label }) => (
-        <Link
-          key={href}
-          href={`/dashboard${href}`}
-          className={cn(
-            "text-sm font-medium transition-colors hover:text-primary",
-            pathname === href ? "text =-black dark:text-white" : "text-muted-foreground"
-          )}
-        >
+    <NavigationMenu className="ml-4">
+      <NavigationMenuList>
+        <NavLinkItem
+          href={storeId ? `/dashboard/${storeId}` : "/dashboard"}
+          label="Overview"
+        />
+        {storeId && (
+          <>
+            <NavLinkItem
+              href={`/dashboard/${storeId}/display`}
+              label="Display"
+            />
+            <NavDropdownItem
+              triggerLabel="Filters"
+              prefix={`/dashboard/${storeId}/filters`}
+              links={[
+                { href: "/category", label: "Category" },
+                { href: "/brand", label: "Brand" },
+              ]}
+            />
+            <NavLinkItem
+              href={`/dashboard/${storeId}/settings`}
+              label="Settings"
+            />
+          </>
+        )}
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
+
+interface NavLinkItemProps {
+  href: string;
+  label: string;
+}
+
+function NavLinkItem({
+  href,
+  label,
+}: NavLinkItemProps) {
+  return (
+    <NavigationMenuItem>
+      <Link href={href} legacyBehavior passHref>
+        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
           {label}
-        </Link>
-      ))}
-    </nav>
+        </NavigationMenuLink>
+      </Link>
+    </NavigationMenuItem>
+  );
+}
+
+interface NavDropdownItemProps {
+  triggerLabel: string;
+  links: NavLinkItemProps[];
+  prefix?: string;
+}
+
+function NavDropdownItem({
+  triggerLabel,
+  links,
+  prefix,
+}: NavDropdownItemProps) {
+  return (
+    <NavigationMenuItem>
+      <NavigationMenuTrigger>{triggerLabel}</NavigationMenuTrigger>
+      <NavigationMenuContent>
+        <div>
+          {links.map(({ href, label }) => (
+            <NavigationMenuLink
+              key={href} 
+              className={navigationMenuTriggerStyle()}
+              asChild
+            >
+              <Link href={prefix ? `${prefix}${href}` : href}>
+                {label}
+              </Link>
+            </NavigationMenuLink>
+          ))}
+        </div>
+      </NavigationMenuContent>
+    </NavigationMenuItem>
   );
 }
