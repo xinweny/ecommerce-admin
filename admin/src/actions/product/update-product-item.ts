a4f1306ddc52f5931bfcacb705b52700bdc615e2
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-04-23 20:50:04 -0400

    add update product item

--- admin/src/actions/product/update-product-item.ts
+++ admin/src/actions/product/update-product-item.ts
@@ -1,12 +1,12 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
+import { extractPublicId } from "cloudinary-build-url";
 
 import { db } from "@/db/client";
+import { cloudinary } from "@/lib/cloudinary";
 
 import { productItemSchema, type ProductItemSchema } from "@/schemas/product";
-
-import { getProductItemImagesByProductItemId } from "@/db/query/product";
 
 export const updateProductItem = async (productItemId: number, values: ProductItemSchema) => {
   try {
@@ -16,12 +16,33 @@ export const updateProductItem = async (productItemId: number, values: ProductIt
 
     const { imageUrls, ...productItemValues } = values;
 
-    const productItem = await db.productItem.update({
-      where: { id: productItemId },
-      data: { ...productItemValues },
-    });
+    const productItem = await db.$transaction(async (tx) => {
+      const productItem = await db.productItem.update({
+        where: { id: productItemId },
+        data: { ...productItemValues },
+      });
 
-    const productItemImages = await getProductItemImagesByProductItemId(productItemId);
+      const images = await tx.productItemImage.findMany({
+        where: { productItemId },
+      });
+
+      const deletedUrls = images
+        .map(image => image.imageUrl)
+        .filter(x => !imageUrls.includes(x));
+
+      await Promise.all([
+        tx.productItemImage.deleteMany({
+          where: { productItemId },
+        }),
+        ...deletedUrls.map(url => cloudinary.uploader.destroy(extractPublicId(url))),
+      ]);
+
+      await tx.productItemImage.createMany({
+        data: imageUrls.map(url => ({ productItemId, imageUrl: url })),
+      });
+
+      return productItem;
+    });
 
     revalidatePath("/products");
 
