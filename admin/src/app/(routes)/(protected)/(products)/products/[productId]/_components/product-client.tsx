75da3f31e45387e7722cf09af6c3f9a16d1d49f7
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-05-09 16:10:24 -0400

    display product info

--- admin/src/app/(routes)/(protected)/(products)/products/[productId]/_components/product-client.tsx
+++ admin/src/app/(routes)/(protected)/(products)/products/[productId]/_components/product-client.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-import { Pencil } from "lucide-react";
-import { useRouter } from "next/navigation";
-
 import { ProductIncludePayload } from "@/db/query/product";
 
-import { Button } from "@/components/ui/button";
-import { Heading } from "@/components/shared/heading";
-import { Separator } from "@/components/ui/separator";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+} from "@/components/ui/card";
+
+import { ProductPageCardHeading } from "./product-page-content";
 
 interface ProductClientProps {
   product: ProductIncludePayload;
@@ -16,9 +17,19 @@ interface ProductClientProps {
 export function ProductClient({
   product,
 }: ProductClientProps) {
-  const router = useRouter();
+  return (
+    <div className="space-y-4">
+      <ProductPageCardHeading>Overview</ProductPageCardHeading>
+      <div className="grid grid-cols-2 gap-4">
+        <ProductInfoCard product={product} />
+      </div>
+    </div>
+  );
+}
 
-  // TODO: display product info
+export function ProductInfoCard({
+  product,
+}: ProductClientProps) {
   const {
     name,
     model,
@@ -30,9 +41,43 @@ export function ProductClient({
     videoUrl,
   } = product;
 
-  return (
-    <>
+  const rows = [
+    { label: "Name", value: name },
+    { label: "Model No.", value: model },
+    { label: "Category", value: category.name },
+    { label: "Subcategory", value: subcategory.name },
+    { label: "Brand", value: brand.name },
+    { label: "Series", value: series?.name || "-" },
+  ];
 
-    </>
+  return (
+    <Card>
+      <CardHeader>
+        <h4 className="font-semibold">Product Info</h4>
+      </CardHeader>
+      <CardContent className="space-y-4 break-all">
+        <div>
+          {rows.map(({ label, value }) => (
+            <div
+              key={label}
+              className="flex gap-2 align-center justify-between text-sm border-b py-2"
+            >
+            <span className="font-semibold break-all">{label}</span>
+            <span className="font-light">{value}</span>
+          </div>
+          ))}
+        </div>
+        <div className="text-sm">
+          <span className=" font-semibold">Description</span>
+          <p className="font-light">{description || "-"}</p>
+        </div>
+        {videoUrl && (
+          <div className="flex flex-col justify-between text-sm">
+            <span className="font-semibold">Video URL</span>
+            <a className="font-light text-blue-600 hover:text-blue-800 visited:text-purple-600" href={videoUrl}>{videoUrl}</a>
+          </div>
+        )}
+      </CardContent>
+    </Card>
   );
 }
