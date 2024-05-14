9f63e4ab80491c23437ec0308660566eb5fd82fa
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-05-14 11:09:45 -0400

    adjust billboard layout

--- store/src/app/(routes)/(store)/category/[categorySlug]/_components/category-billboard.tsx
+++ store/src/app/(routes)/(store)/category/[categorySlug]/_components/category-billboard.tsx
@@ -13,17 +13,19 @@ export function CategoryBillboard({
     <div className="space-y-4 p-4 sm:p-6 lg:p-8">
       <div className="relative rounded-xl overflow-hidden ">
         <div
-          className="h-full w-full rounded-xl aspect-square md:aspect-[2.4/1 overflow-hidden bg-cover"
+          className="h-full w-full rounded-xl aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover bg-blue-200"
           style={{
             backgroundImage: `url(${billboard
               ? billboard.imageUrl
               : "#"})`,
-            filter: "brightness(75%)",
+            filter: "brightness(50%)",
           }}
         />
-        <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-bold text-2xl sm:text-4xl lg:text-5xl sm:max-w-xl max-w-xs drop-shadow-lg">{name}</h1>
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white space-y-4 lg:space-y-8 text-center drop-shadow-lg">
+          <h1 className="font-bold text-2xl sm:text-4xl lg:text-5xl sm:max-w-xl max-w-xs">{name}</h1>
+          {billboard?.description && <p className="text-lg sm:text-md lg:text-2xl sm:max-w-xl">{billboard.description}</p>}
+        </div>
       </div>
-      {billboard && <p className="text-sm">{billboard.description}</p>}
     </div>
   );
 }
